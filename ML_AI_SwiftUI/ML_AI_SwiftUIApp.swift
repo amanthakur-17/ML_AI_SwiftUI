8abb34a305cff54da88c4b51c6a3509a1c5ec7b3
Aman <aman@Amans-MacBook-Pro.local>
2025-07-29 15:07:51 +0530

    initial commit

---
+++ ML_AI_SwiftUI/ML_AI_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ML_AI_SwiftUIApp.swift
+//  ML_AI_SwiftUI
+//
+//  Created by Aman on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ML_AI_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
